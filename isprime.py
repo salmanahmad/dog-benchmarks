faa09221d081b72605c79cf97cc0d2a736f9d847
Samarth Mohan <smohan94@mit.edu>
2013-03-28 00:05:48 -0400

    isprime.py implementation

---
+++ isprime.py
@@ -0,0 +1,14 @@
+
+
+def isprime(n):
+    n*=1.0
+    if n%2==0 and n!=2 or n%3==0 and n!=3:
+        return False
+    for b in range(1,int((n**0.5+1)/6.0+1)):
+        if n%(6*b-1)==0:
+            return False
+        if n %(6*b+1)==0:
+           return False
+    return True
+a = isprime(13000081)
+print a    
